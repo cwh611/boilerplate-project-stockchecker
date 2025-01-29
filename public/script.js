fb8e65ec546d8cc07c9254e2217ec98108ff1064
cwh611 <chuck@MacBookPro.lan>
2025-01-28 16:54:21 -0800

    updates

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 document.getElementById("submit-btn").addEventListener("click", () => {
-  const stock_1 = document.getElementById("stock-1-input").value.trim;
-  const stock_2 = document.getElementById("stock-2-input").value.trim;
+  const stock_1 = document.getElementById("stock-1-input").value;
+  const stock_2 = document.getElementById("stock-2-input").value;
   if (!stock_1 && !stock_2) return alert("Please input at least one stock");
   const baseUrl = new URL('https://chunk-stockchecker-4201c325fca9.herokuapp.com/api/stock-prices?');
   let url = `${baseUrl}stock=${stock_1}`
