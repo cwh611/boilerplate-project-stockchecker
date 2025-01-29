ab55e9b0d1c4544e73356e60a9c22c5d6b429439
cwh611 <chuck@MacBookPro.lan>
2025-01-28 17:13:40 -0800

    updates

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const output = document.getElementById("results-container");
+
 document.getElementById("submit-btn").addEventListener("click", () => {
   const stock_1 = document.getElementById("stock-1-input").value;
   const stock_2 = document.getElementById("stock-2-input").value;
@@ -12,8 +14,43 @@ document.getElementById("submit-btn").addEventListener("click", () => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      console.log('Response:', data);
-      document.getElementById("results-container").innerText = JSON.stringify(data)
+      const result = JSON.stringify(data);
+      if (!Array.isArray(result.stockData)) {
+        output.innerHTML = `
+          <div>
+            $${result.stockData.symbol}
+          </div>
+          <div>
+            $${result.stockData.latestPrice}
+          </div>
+          <div>
+            Likes: ${result.stockData.likes}
+          </div>`
+      } else {
+        output.innerHTML = `
+          <div>
+            <div>
+              $${result.stockData[0].symbol}
+            </div>
+            <div>
+              $${result.stockData[0].latestPrice}
+            </div>
+            <div>
+              Likes: ${result.stockData[0].rel_likes}
+            </div>
+          </div>
+          <div>
+            <div>
+              $${result.stockData[1].symbol}
+            </div>
+            <div>
+              $${result.stockData[1].latestPrice}
+            </div>
+            <div>
+              Likes: ${result.stockData[1].rel_likes}
+            </div>
+          </div>`
+      }
     })
     .catch(error => {
       console.error('Error fetching stock prices:', error);
